be317cd523e2870930515c49d117b7a00b4fd0dc
antun <antun.jerkovic@gmail.com>
2020-08-14 12:32:34 +0200

    Malo osvjezenje nakon dugog bavljenja sa virtenvomgit status2

--- gizhub.py
+++ gizhub.py
@@ -4,6 +4,6 @@ print('Antun je napravio prvi pull!!!')
 # sljedeca linija koda namajenjena je za mali_dodatak branch vjezbu
 if True:
     print('Moja prva promjena unutar brancha!!!')
-    print('Nova promjena nakon dugo vremna!')
+    print('Nova promjena nakon dugo vremna!git ')
 
 # Komentar za provociranje gita
